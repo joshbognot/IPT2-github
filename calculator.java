d795f11244f9558867cbe7e73f1ee13650b0ceb4
Josh <joshiwnl24@gmail.com>
2024-09-12 13:51:59 +0800

    Adding the addition method

--- calculator.java
+++ calculator.java
@@ -1,5 +1,10 @@
 public class calculator {
+    public static int add(int a, int b) {
+        return a + b;
+     }
+
     public static void main(String[] args) {
         System.out.println("Calculator: ");
+        System.out.println("Addition: " + add(5, 3));
     }
 }
